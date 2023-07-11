6adfcaa9159273597602f185e5632a1157bbf58b
Quarterpounderwithcheese <92781713+Quarterpounderwithcheese@users.noreply.github.com>
2023-07-12 01:07:13 +0300

    task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,6 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-FDD_weight_MD = 1.44
+FDD_weight_MB = 1.44
 pages_ = 100
 lines_ = 50
 symbols_ = 25
@@ -10,6 +10,6 @@ one_KB = 1024
 
 one_page_weight_B = lines_ * symbols_ * one_symbol_weight_B
 one_book_weight_B = pages_ * one_page_weight_B
-FDD_weight_B = FDD_weight_MD * one_MB * one_KB
+FDD_weight_B = FDD_weight_MB * one_MB * one_KB
 total_amount_of_books = FDD_weight_B // one_book_weight_B
 print("Количество книг, помещающихся на дискету:", round(total_amount_of_books))
